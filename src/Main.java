ae5c3584e92fc78af7661f08c074982b4e01c95a
numme <nummer14@naver.com>
2025-06-18 10:19:56 +0900

    Thirdd commit, remove

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,14 @@ public class Main {
         fruits.add("Mango");
         fruits.add("Bbanana");
         fruits.add("Apple");
+        fruits.add("Tomato");
         fruits.add("Cherry");
         fruits.add("Banana");
+        fruits.add("Potato");
         fruits.add("Dragonfruit");
+
+        fruits.remove("Tomato");
+        fruits.remove(5);
 
         if(fruits.contains("Kiwi")) {
             System.out.println("Kiwi는 포함되어 있습니다.");
