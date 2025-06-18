7dafea8f8d79b93cfbbf71158b2a76f602d2dcb6
numme <nummer14@naver.com>
2025-06-18 10:02:31 +0900

    Second commit, CollectionS

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> fruits = new ArrayList<>();
 
-        fruits.add("Apple");
-        fruits.add("Banana");
-        fruits.add("Cherry");
-        fruits.add("Dragonfruit");
         fruits.add("Mango");
+        fruits.add("Apple");
+        fruits.add("Cherry");
+        fruits.add("Banana");
+        fruits.add("Dragonfruit");
 
-//        for(int i = 0; i < fruits.size(); i++) {
-//            System.out.println(fruits.get(i));
-//        }
+        Collections.sort(fruits);
+
         for(String fruit: fruits) {
             System.out.println(fruit);
         }
